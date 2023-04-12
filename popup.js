9ac7b20427fb407cba770dbb4acf12011be33f60
Felipe Sbragia <freelancer_Sbragia@mfmti.com.br>
2023-04-12 14:59:52 -0300

    Refatoração

--- popup.js
+++ popup.js
@@ -1,90 +1,80 @@
-console.log('Popup foi aberto!'); // Adicionando um console.log() para verificar se o arquivo está sendo executado
+document.addEventListener('DOMContentLoaded', () => {
+  const captureButton = document.getElementById('capture');
 
-function getReports() {
-  chrome.storage.local.get('reports', function(data) {
-    var reports = data.reports || [];
-    var reportList = document.getElementById('report-list');
-    reportList.innerHTML = '';
-    reports.forEach(function(report) {
-      var tr = document.createElement('tr');
-      tr.addEventListener('click', function() {
-        chrome.downloads.show(report.downloadId);
-      });
-      var tdTimestamp = document.createElement('td');
-      tdTimestamp.innerText = report.timestamp;
-      tr.appendChild(tdTimestamp);
-      var tdTitle = document.createElement('td');
-      tdTitle.innerText = report.title;
-      tr.appendChild(tdTitle);
-      var tdUrl = document.createElement('td');
-      tdUrl.innerText = report.url;
-      tr.appendChild(tdUrl);
-      var tdScreenshot = document.createElement('td');
-      var imgScreenshot = document.createElement('img');
-      imgScreenshot.src = report.screenshotUrl;
-      tdScreenshot.appendChild(imgScreenshot);
-      tr.appendChild(tdScreenshot);
-      var tdLink = document.createElement('td');
-      var aLink = document.createElement('a');
-      aLink.href = report.filename;
-      aLink.innerText = report.filename;
-      tdLink.appendChild(aLink);
-      tr.appendChild(tdLink);
-      reportList.appendChild(tr);
-    });
+  captureButton.addEventListener('click', async () => {
+    const dataUrl = await captureScreen();
+    saveScreenshot(dataUrl);
+    generateReport();
   });
-}
 
-document.addEventListener('DOMContentLoaded', function() {
-  getReports();
-  chrome.storage.onChanged.addListener(getReports);
-});
+  function captureScreen() {
+    return html2canvas(document.documentElement);
+  }
 
-document.addEventListener("DOMContentLoaded", () => {
-  function initializePopup() {
-    const captureButton = document.getElementById("capture");
-    captureButton.addEventListener("click", async () => {
-      const dataUrl = await captureScreen();
-      saveScreenshot(dataUrl);
-      generateReport();
+  function saveScreenshot(dataUrl) {
+    chrome.downloads.download({
+      url: dataUrl,
+      filename: 'screenshot.png',
     });
   }
 
-  initializePopup();
+  function generateReport() {
+    const report = {
+      url: window.location.href,
+      title: document.title,
+      timestamp: new Date().toLocaleString(),
+    };
+
+    chrome.storage.local.get('reports', function (data) {
+      const reports = data.reports || [];
+      reports.unshift(report);
+      chrome.storage.local.set({ reports });
+    });
+  }
+
+  function updateReportList() {
+    chrome.storage.local.get('reports', function (data) {
+      const reports = data.reports || [];
+
+      const reportList = document.getElementById('report-list');
+      reportList.innerHTML = '';
+
+      for (const report of reports) {
+        const tr = document.createElement('tr');
+        tr.addEventListener('click', function () {
+          chrome.downloads.show(report.downloadId);
+        });
+
+        const tdTimestamp = document.createElement('td');
+        tdTimestamp.innerText = report.timestamp;
+        tr.appendChild(tdTimestamp);
+
+        const tdTitle = document.createElement('td');
+        tdTitle.innerText = report.title;
+        tr.appendChild(tdTitle);
+
+        const tdUrl = document.createElement('td');
+        tdUrl.innerText = report.url;
+        tr.appendChild(tdUrl);
+
+        const tdScreenshot = document.createElement('td');
+        const imgScreenshot = document.createElement('img');
+        imgScreenshot.src = report.screenshotUrl;
+        tdScreenshot.appendChild(imgScreenshot);
+        tr.appendChild(tdScreenshot);
+
+        const tdLink = document.createElement('td');
+        const aLink = document.createElement('a');
+        aLink.href = report.filename;
+        aLink.innerText = report.filename;
+        tdLink.appendChild(aLink);
+        tr.appendChild(tdLink);
+
+        reportList.appendChild(tr);
+      }
+    });
+  }
+
+  updateReportList();
+  chrome.storage.onChanged.addListener(updateReportList);
 });
-
-async function captureScreen() {
-  const tab = await getCurrentTab();
-  const dataUrl = await html2canvas(tab);
-  return dataUrl;
-}
-
-function saveScreenshot(dataUrl) {
-  chrome.downloads.download({
-    url: dataUrl,
-    filename: "screenshot.png"
-  });
-}
-
-function generateReport() {
-  const report = {
-    url: window.location.href,
-    title: document.title,
-    date: new Date().toLocaleString()
-  };
-  chrome.runtime.sendMessage({ action: "generateReport", report });
-}
-
-captureButton.addEventListener("click", async () => {
-  console.log("Botão de captura clicado!");
-  const dataUrl = await captureScreen();
-  saveScreenshot(dataUrl);
-  generateReport();
-});
-
-async function captureScreen() {
-  const tab = await getCurrentTab();
-  const dataUrl = await html2canvas(tab);
-  console.log(dataUrl);
-  return dataUrl;
-}
